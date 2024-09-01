ac863fcc144010f063e3ad47b72cfec723dbe8a0
Varun Dunna <varun.dunna2524@gmail.com>
2024-09-01 21:04:57 +0530

    delete-functionality-added

--- todo-backend.js
+++ todo-backend.js
@@ -5,13 +5,14 @@ app.use(express.json());
 let log = console.log;
 app.get("/", function (req, res) {
   fs.readFile("todos.json", "utf-8", function (err, data) {
-    if (err) {
+    if (err || data.trim() == "") {
       let todos = [];
       fs.writeFile("todos.json", JSON.stringify(todos), () => {
         res.json({ todoList: todos, msg: "Todo list is empty." });
       });
     } else {
-      res.json({ data });
+      log(data);
+      res.json({ data: JSON.parse(data) });
     }
   });
 });
@@ -63,7 +64,7 @@ app.post("/", function (req, res) {
             const todoToBeAdded = todo;
             const smallerObject = {
               todoID: todoID,
-              todoToBeAdded: todoToBeAdded,
+              todo: todoToBeAdded,
             };
             temp[i].todos.push(smallerObject);
             let data = temp;
@@ -95,4 +96,48 @@ app.post("/", function (req, res) {
     }
   });
 });
-app.listen(3000);
+app.delete("/", function (req, res) {
+  const id = req.body.id;
+  const todo = req.body.todo;
+  fs.readFile("todos.json", "utf-8", (err, data) => {
+    if (err || data == "[]" || data.trim() == "") {
+      res.json({ msg: "File not found or file is empty. Cannot delete" });
+      return;
+    } else {
+      let jsonData = JSON.parse(data);
+      let FOUND = 0;
+      for (let i = 0; i < jsonData.length; i++) {
+        if (jsonData[i].id == id) {
+          // log("here");
+          let found = 0;
+          let todoList = jsonData[i].todos;
+          // if(todoList)
+          for (let j = 0; j < todoList.length; j++) {
+            if (todoList[j].todo == todo) {
+              todoList.splice(j, 1);
+              jsonData[i].todos = todoList;
+              fs.writeFile("todos.json", JSON.stringify(jsonData), () => {
+                res.send("Deleted.");
+              });
+              found = 1;
+              FOUND = 1;
+              break;
+            }
+          }
+          if (found == 0) {
+            FOUND = 1;
+            res.send("Couldnt find todo for that ID.");
+            break;
+          }
+        }
+      }
+      if (FOUND == 0) {
+        res.send("Couldnt find ID");
+        return;
+      }
+    }
+  });
+});
+app.listen(3000, () => {
+  log("Listening on 3000");
+});
