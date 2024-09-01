d6284e49f4bf0648efe72f4deb93df96a3142f2e
Varun Dunna <varun.dunna2524@gmail.com>
2024-09-01 22:34:57 +0530

    project-done

--- todo-backend.js
+++ todo-backend.js
@@ -20,6 +20,45 @@ app.get("/", function (req, res) {
 /*
     [{id:1,todos:[{todoID:1,todo:"kek"},{todoID:2,todo:"kek2"}]},{id:2,todos:[{todoID:1}...]}]
 */
+app.put("/", function (req, res) {
+  fs.readFile("todos.json", "utf-8", function (err, data) {
+    if (err) {
+      res.json({ message: "File not found." });
+      return;
+    } else {
+      const id = req.body.id;
+      const oldTodo = req.body.oldTodo;
+      const newTodo = req.body.newTodo;
+      const jsonData = JSON.parse(data);
+      let FOUND = 0;
+      for (let i = 0; i < jsonData.length; i++) {
+        if (jsonData[i].id == id) {
+          let todoList = jsonData[i].todos;
+          let found = 0;
+          for (let j = 0; j < todoList.length; j++) {
+            if (todoList[j].todo == oldTodo) {
+              todoList[j].todo = newTodo;
+              jsonData[i].todos = todoList;
+              fs.writeFile("todos.json", JSON.stringify(jsonData), () => {
+                res.json({ message: "Done" });
+              });
+              found = 1;
+              FOUND = 1;
+            }
+          }
+          if (found == 0) {
+            FOUND = 1;
+            res.json({ message: "Todo not found for that ID." });
+            break;
+          }
+        }
+      }
+      if (FOUND == 0) {
+        res.json({ message: "ID not found." });
+      }
+    }
+  });
+});
 app.post("/", function (req, res) {
   console.log(req.body);
   const id = req.body.id;
